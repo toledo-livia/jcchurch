c3ca82e23569803c53c480363b45e83eb45af7da
Lívia Toledo <elitgroupstarter@gmail.com>
2023-11-02 16:34:07 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import logo from './logo.png';
+import { FaLocationDot, FaEnvelope, FaPhone } from "react-icons/fa6";
+import { FaWhatsapp, FaInstagram, FaYoutube, FaTiktok, FaSpotify } from "react-icons/fa6";
 import './App.css';
 
 function App() {
@@ -6,18 +8,74 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">
+          JC Church
+        </h1>
+        <span className="App-subtitle">
+          Tocados para transformar
+        </span>
       </header>
+      <div className="App-container">
+        <FaPhone size={20} color="#ffffff" />
+        <FaEnvelope size={20} color="#ffffff" />
+        <FaLocationDot size={20} color="#ffffff" />
+      </div>
+      <div className="App-content">
+        <div className="App-box">
+          <FaWhatsapp size={35} color="#ffffff" />
+          <div className="App-content-box">
+            <h2 className="App-box-title">WhatsApp Oficial</h2>
+            <span className="App-box-description">
+              Contato | Agenda | Informações
+            </span>
+          </div>
+        </div>
+        <div className="App-box">
+          <FaInstagram size={35} color="#ffffff" />
+          <div className="App-content-box">
+            <h2 className="App-box-title">Instagram Oficial</h2>
+            <span className="App-box-description">
+              @jcchurch.br
+            </span>
+          </div>
+        </div>
+        <div className="App-box">
+          <FaYoutube size={35} color="#ffffff" />
+          <div className="App-content-box">
+            <h2 className="App-box-title">Canal Oficial YouTube</h2>
+            <span className="App-box-description">
+              @JCChurchBR
+            </span>
+          </div>
+        </div>
+        <div className="App-box">
+          <FaTiktok size={35} color="#ffffff" />
+          <div className="App-content-box">
+            <h2 className="App-box-title">Tiktok JC Oficial</h2>
+            <span className="App-box-description">
+              jcchurch.br
+            </span>
+          </div>
+        </div>
+        <div className="App-box">
+          <FaTiktok size={35} color="#ffffff" />
+          <div className="App-content-box">
+            <h2 className="App-box-title">Tiktok Essence Oficial</h2>
+            <span className="App-box-description">
+              essence.jc1
+            </span>
+          </div>
+        </div>
+        <div className="App-box">
+          <FaSpotify size={35} color="#ffffff" />
+          <div className="App-content-box">
+            <h2 className="App-box-title">Spotify Oficial</h2>
+            <span className="App-box-description">
+              Playlist
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
